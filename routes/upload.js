c2f4a97a244c128dabc3fc08a3aa1f71a7e41e28
fredpineau <97562669+fredpineau@users.noreply.github.com>
2025-11-24 22:55:10 +0100

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,9 @@
 import express from "express";
 import upload from "../config/multer.js";
-import { uploadFile } from "../controllers/uploadController.js";
+import uploadFileWithContact from "../controllers/uploadController.js";
 
 const router = express.Router();
 
-router.post("/", upload.single("file"), uploadFile, controller.uploadFileWithContact);
+router.post("/", upload.single("file"), uploadFileWithContact, controller.uploadFileWithContact);
 
 export default router;
